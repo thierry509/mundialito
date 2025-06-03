0a36245a109f80192d8771606c9b1b07486dff8a
Thierry Desir <81268651+thierry509@users.noreply.github.com>
2025-06-03 07:22:49 -0400

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+<meta name="robots" content="noindex, nofollow">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="{{ asset('images/mundialito.ico') }}" type="image/x-icon">
