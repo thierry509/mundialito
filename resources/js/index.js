bef90029addae37104370edf67313d15f0a631fb
thierry509 <desirthierry2015@gmail.com>
2025-08-23 14:18:59 -0400

    fjjf

--- resources/js/index.js
+++ resources/js/index.js
@@ -9,12 +9,13 @@ window.Pusher = Pusher
 const forceTLS = import.meta.env.VITE_FORCE_TLS === 'true'; // convertit string en booléen
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: forceTLS,
+    wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     enabledTransports: ['ws', 'wss'],
 })
 
@@ -65,7 +66,6 @@ document.addEventListener('alpine:init', () => {
 
             const channelName = `comments.${this.type}.${this.id}`
 
-            console.log("Echo chargé ?", window.Echo)
 
             window.Echo.channel(channelName)
                 .listen('CommentPosted', (e) => {
